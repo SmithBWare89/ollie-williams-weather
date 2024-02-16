a5d6b1b4e62607f52670a197673c440021459698
Stephon Smith <Smithwrestling89@gmail.com>
2024-02-16 14:49:59 -0500

    Working icons

--- ollie-williams-weather/src/app/components/search-tool/search-tool.component.ts
+++ ollie-williams-weather/src/app/components/search-tool/search-tool.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import {
+  faMagnifyingGlass,
+  IconDefinition,
+} from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-search-tool',
   standalone: true,
-  imports: [],
+  imports: [FontAwesomeModule],
   templateUrl: './search-tool.component.html',
-  styleUrl: './search-tool.component.css'
+  styleUrl: './search-tool.component.css',
 })
 export class SearchToolComponent {
-
+  public magnifyingGlass: IconDefinition = faMagnifyingGlass;
 }
