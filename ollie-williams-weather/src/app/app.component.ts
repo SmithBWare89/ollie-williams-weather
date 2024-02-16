1baef191207dbc0b1fed64c63e230323a8f5646e
Stephon Smith <Smithwrestling89@gmail.com>
2024-02-16 13:50:28 -0500

    Stopping point before adding icons

--- ollie-williams-weather/src/app/app.component.ts
+++ ollie-williams-weather/src/app/app.component.ts
@@ -2,11 +2,17 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
 import { SearchToolComponent } from './components/search-tool/search-tool.component';
+import { ResultsContainerComponent } from './components/results-container/results-container.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, SearchToolComponent],
+  imports: [
+    RouterOutlet,
+    HeaderComponent,
+    SearchToolComponent,
+    ResultsContainerComponent,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
---
+++ ollie-williams-weather/src/app/components/results-container/results-container.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import {SearchToolComponent} from "../search-tool/search-tool.component";
+
+@Component({
+  selector: 'app-results-container',
+  standalone: true,
+  imports: [SearchToolComponent],
+  templateUrl: './results-container.component.html',
+  styleUrl: './results-container.component.css',
+})
+export class ResultsContainerComponent {}
