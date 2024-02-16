07f9600b0620bdcfce320f4a15356b44734ab69e
Stephon Smith <Smithwrestling89@gmail.com>
2024-02-16 15:41:56 -0500

    Able to save the searched city to an observable that emits

--- ollie-williams-weather/src/app/components/search-tool/search-tool.component.ts
+++ ollie-williams-weather/src/app/components/search-tool/search-tool.component.ts
@@ -1,9 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter } from '@angular/core';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import {
   faMagnifyingGlass,
   IconDefinition,
 } from '@fortawesome/free-solid-svg-icons';
+import { BehaviorSubject } from 'rxjs';
 
 @Component({
   selector: 'app-search-tool',
@@ -14,4 +15,14 @@ import {
 })
 export class SearchToolComponent {
   public magnifyingGlass: IconDefinition = faMagnifyingGlass;
+  private searchedCity$: BehaviorSubject<string | undefined> =
+    new BehaviorSubject<string | undefined>(undefined);
+  private searchedCityEvent: EventEmitter<string> = new EventEmitter<string>();
+  public onChange(city: string) {
+    this.searchedCity$.next(city.trim());
+    this.emitCity();
+  }
+  private emitCity() {
+    this.searchedCityEvent.emit(this.searchedCity$.value);
+  }
 }
