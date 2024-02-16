678557cee5849b4fb664a45c328dc51257aa7aa8
Stephon Smith <Smithwrestling89@gmail.com>
2024-02-16 12:14:08 -0500

    Added beginning of search tool

--- ollie-williams-weather/src/app/app.component.ts
+++ ollie-williams-weather/src/app/app.component.ts
@@ -1,14 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import {HeaderComponent} from "./components/header/header.component";
+import { HeaderComponent } from './components/header/header.component';
+import { SearchToolComponent } from './components/search-tool/search-tool.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [RouterOutlet, HeaderComponent, SearchToolComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
-export class AppComponent {
-  title = 'ollie-williams-weather';
-}
+export class AppComponent {}
