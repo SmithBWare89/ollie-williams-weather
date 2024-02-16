e2bf36af4495c09107a2af68c2817e4c8ed06add
Stephon Smith <Smithwrestling89@gmail.com>
2024-02-16 12:06:54 -0500

    Added tailwind config customization as well as creating header component

--- ollie-williams-weather/tailwind.config.js
+++ ollie-williams-weather/tailwind.config.js
@@ -3,6 +3,17 @@ module.exports = {
   content: ["./src/**/*.{html,js,ts}"],
   theme: {
     extend: {},
+    backgroundColor: {
+      low: 'darkgreen',
+      moderate: 'yellow',
+      high: 'darkorange',
+      veryHigh: 'red',
+      extreme: '#800080',
+      default: '#4d5dfb'
+    },
+    backgroundImage: {
+      gradientPattern: 'linear-gradient(315deg, #4d5dfb 0%, #08c8f6 74%)'
+    }
   },
   plugins: [],
 }
