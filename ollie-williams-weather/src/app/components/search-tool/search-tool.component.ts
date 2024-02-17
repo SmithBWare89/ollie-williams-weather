cad65561da0fc0199172f6fe8cd692a8495a6030
Stephon Smith <Smithwrestling89@gmail.com>
2024-02-16 21:58:57 -0500

    Updated validation for non text cities

--- ollie-williams-weather/src/app/components/search-tool/search-tool.component.ts
+++ ollie-williams-weather/src/app/components/search-tool/search-tool.component.ts
@@ -4,28 +4,36 @@ import {
   faMagnifyingGlass,
   IconDefinition,
 } from '@fortawesome/free-solid-svg-icons';
-import { BehaviorSubject } from 'rxjs';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-search-tool',
   standalone: true,
-  imports: [FontAwesomeModule],
+  imports: [FontAwesomeModule, CommonModule],
   templateUrl: './search-tool.component.html',
   styleUrl: './search-tool.component.css',
 })
 export class SearchToolComponent {
   public magnifyingGlass: IconDefinition = faMagnifyingGlass;
-  private regEx: RegExp = /\w+/g;
+  private regEx: RegExp = /^[A-Za-z ]*$/g;
   private searchedCity$: BehaviorSubject<string | undefined> =
-    new BehaviorSubject<string | undefined>(undefined);
+    new BehaviorSubject<string | undefined>('');
+  private _error$: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(
+    false,
+  );
+  public error$: Observable<boolean> = this._error$.asObservable();
   @Output() searchedCityEvent: EventEmitter<string | undefined> =
     new EventEmitter<string | undefined>();
   public onChange(city: string): void {
+    this._error$.next(false);
     if (this.validateSearchedCity(city)) {
       this.searchedCity$.next(city.trim());
+      this.emitCity();
+      return;
     }
     this.searchedCity$.next(undefined);
-    this.emitCity();
+    this._error$.next(true);
   }
   private emitCity(): void {
     this.searchedCityEvent.emit(this.searchedCity$.value);
