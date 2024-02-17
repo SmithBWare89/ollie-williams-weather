7597d3e1f61dd9b7a36322d0fc6d16bc855517d0
Stephon Smith <Smithwrestling89@gmail.com>
2024-02-17 13:46:07 -0500

    Stopping point. About to add cities being displayed

--- ollie-williams-weather/src/app/components/recent-search/recent-search.component.ts
+++ ollie-williams-weather/src/app/components/recent-search/recent-search.component.ts
@@ -1,12 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'app-recent-search',
   standalone: true,
-  imports: [],
+  imports: [AsyncPipe],
   templateUrl: './recent-search.component.html',
-  styleUrl: './recent-search.component.css'
+  styleUrl: './recent-search.component.css',
 })
 export class RecentSearchComponent {
-
+  @Input() searchedCities(cities: string[] | undefined) {
+    if (!cities) return;
+    this._cities$.next(cities);
+  }
+  private _cities$: BehaviorSubject<string[]> = new BehaviorSubject<string[]>(
+    [],
+  );
+  public cities$: Observable<string[]> = this._cities$.asObservable();
 }
--- ollie-williams-weather/src/app/components/results-container/results-container.component.ts
+++ ollie-williams-weather/src/app/components/results-container/results-container.component.ts
@@ -1,18 +1,18 @@
 import { Component } from '@angular/core';
 import { SearchToolComponent } from '../search-tool/search-tool.component';
 import { ResultsContainerService } from './results-container.service';
-import { RecentSearchContainerComponent } from '../recent-search-container/recent-search-container.component';
+import { RecentSearchComponent } from '../recent-search/recent-search.component';
 
 @Component({
   selector: 'app-results-container',
   standalone: true,
-  imports: [SearchToolComponent, RecentSearchContainerComponent],
+  imports: [SearchToolComponent, RecentSearchComponent],
   templateUrl: './results-container.component.html',
   styleUrl: './results-container.component.css',
 })
 export class ResultsContainerComponent {
   constructor(private resultsContainerService: ResultsContainerService) {}
-  public searchForCity(city: string) {
+  public addSearchedCity(city: string) {
     this.resultsContainerService.addSearchedCity(city);
   }
 }
