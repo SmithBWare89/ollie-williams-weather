86a16b3d7cb817945ea716c5e601bf5208d3bed1
Stephon Smith <Smithwrestling89@gmail.com>
2024-02-19 12:43:18 -0500

    Added hero and formatting updates

---
+++ ollie-williams-weather/src/app/components/forecast/forecast.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-forecast',
+  standalone: true,
+  imports: [],
+  templateUrl: './forecast.component.html',
+  styleUrl: './forecast.component.css'
+})
+export class ForecastComponent {
+
+}
