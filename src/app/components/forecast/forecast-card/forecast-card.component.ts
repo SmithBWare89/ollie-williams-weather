d778f4a084236d26ac359aabb4d4b8d08a66510b
Stephon Smith <Smithwrestling89@gmail.com>
2024-03-02 11:35:37 -0500

    Displaying forecast from API results

--- src/app/components/forecast/forecast-card/forecast-card.component.ts
+++ src/app/components/forecast/forecast-card/forecast-card.component.ts
@@ -1,12 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { WeatherForecastType } from '../../../shared/shared.types';
+import { DatePipe, NgIf } from '@angular/common';
+import { convertDate } from '../../../shared/shared.utility';
 
 @Component({
   selector: 'app-forecast-card',
   standalone: true,
-  imports: [],
+  imports: [NgIf, DatePipe],
   templateUrl: './forecast-card.component.html',
-  styleUrl: './forecast-card.component.css'
+  styleUrl: './forecast-card.component.css',
 })
 export class ForecastCardComponent {
-
+  @Input() forecast: WeatherForecastType | undefined = undefined;
+  protected readonly convertDate = convertDate;
 }
--- src/app/components/forecast/forecast.component.ts
+++ src/app/components/forecast/forecast.component.ts
@@ -1,11 +1,12 @@
 import { Component, Input } from '@angular/core';
 import { ForecastCardComponent } from './forecast-card/forecast-card.component';
 import { WeatherForecastType } from '../../shared/shared.types';
+import { NgForOf, SlicePipe } from '@angular/common';
 
 @Component({
   selector: 'app-forecast',
   standalone: true,
-  imports: [ForecastCardComponent],
+  imports: [ForecastCardComponent, NgForOf, SlicePipe],
   templateUrl: './forecast.component.html',
   styleUrl: './forecast.component.css',
 })
