85aa749be9803bd41f1497187ce5d6a6f565a07c
Stephon Smith <Smithwrestling89@gmail.com>
2024-03-06 18:30:43 -0500

    Adjusted hero and forecast cards for md screen

--- src/app/components/hero/hero.component.ts
+++ src/app/components/hero/hero.component.ts
@@ -2,11 +2,12 @@ import { Component, Input } from '@angular/core';
 import { CurrentWeatherType } from '../../shared/shared.types';
 import { CommonModule, DatePipe } from '@angular/common';
 import { convertDate } from '../../shared/shared.utility';
+import { WeatherIconComponent } from '../weather-icon/weather-icon.component';
 
 @Component({
   selector: 'app-hero',
   standalone: true,
-  imports: [CommonModule, DatePipe],
+  imports: [CommonModule, DatePipe, WeatherIconComponent],
   templateUrl: './hero.component.html',
   styleUrl: './hero.component.css',
 })
